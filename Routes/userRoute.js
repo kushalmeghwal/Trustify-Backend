72c6be010a04b7466027c06270fae1b926a1ad87
Devendra Suthiya <sharmadev7004@gmail.com>
2025-04-30 23:02:06 +0530

    bug fixed in product fetching

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginUser, registerUser, updateContactsList, verifyUserForPasswordReset, resetPassword, getUserProfile, updateUserProfile, updateProfileImage, updateUserPassword } from "../Controllers/userController.js";
+import { loginUser, registerUser, updateContactsList, verifyUserForPasswordReset, resetPassword, updateProfileImage, updateUserPassword } from "../Controllers/userController.js";
 import { verifyToken } from "../Middlewares/auth.js";
 
 const router = express.Router();
@@ -11,8 +11,6 @@ router.post("/verify-reset", verifyUserForPasswordReset);
 router.post("/reset-password", resetPassword);
 
 // Protected routes
-router.get("/user/profile", verifyToken, getUserProfile);
-router.put("/user/profile", verifyToken, updateUserProfile);
 router.put("/user/profile/image", verifyToken, updateProfileImage);
 router.put("/user/password", verifyToken, updateUserPassword);
 router.post("/updateContactList", verifyToken, updateContactsList);
