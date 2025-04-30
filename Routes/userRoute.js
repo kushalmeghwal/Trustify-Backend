00d5f27de3955d49b7c5cffee93b4465f8eafee0
Devendra Suthiya <sharmadev7004@gmail.com>
2025-04-30 15:50:22 +0530

    check on email and edit user profile

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { loginUser, registerUser, updateContactsList } from "../Controllers/userController.js";
+import { loginUser, registerUser, updateContactsList, verifyUserForPasswordReset, resetPassword, getUserProfile, updateUserProfile, updateProfileImage, updateUserPassword } from "../Controllers/userController.js";
+import { verifyToken } from "../Middlewares/auth.js";
 
 const router = express.Router();
 
+// Public routes
 router.post("/login", loginUser);
 router.post("/register", registerUser);
-router.post("/updateContactList", updateContactsList);
+router.post("/verify-reset", verifyUserForPasswordReset);
+router.post("/reset-password", resetPassword);
+
+// Protected routes
+router.get("/user/profile", verifyToken, getUserProfile);
+router.put("/user/profile", verifyToken, updateUserProfile);
+router.put("/user/profile/image", verifyToken, updateProfileImage);
+router.put("/user/password", verifyToken, updateUserPassword);
+router.post("/updateContactList", verifyToken, updateContactsList);
 
 export default router;
